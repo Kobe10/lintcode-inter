ddb65082b46868b299d4a1b36289a2ffea2ee4db
fuzq <fuzq@bjshfb.com>
2019-05-06 13:19:41 +0800

    Initial commit

---
+++ src/main/java/com/fzq/lintcode/stack/isValidBracket.java
@@ -0,0 +1,23 @@
+package com.fzq.lintcode.stack;
+
+/**
+ * @program: lintcode-inter
+ * @description: 有效的括号
+ * @author: fuzq
+ * @create: 2018-11-22 10:52
+ **/
+public class isValidBracket {
+    /**
+     * @Description :
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+     *
+     * 有效字符串需满足：
+     *      1、左括号必须用相同类型的右括号闭合。
+     *      2、左括号必须以正确的顺序闭合。
+     *      注意空字符串可被认为是有效字符串。
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+}
